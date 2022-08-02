2f582e2a0565f953fd00deceb7a5d72867b17416
Ausus <>
2022-08-02 15:58:21 +0000

    Initial commit

---
+++ Events/Interaction/interactionCreate.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "interactionCreate",
+  /**
+  * @param {Client} client
+  **/
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const { GatewayIntentBits, Partials, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection } = require('discord.js');
+
+const { REST } = require("@discordjs/rest");
+const token = process.env['TOKEN'];
+
+
+
+//console.log(GatewayIntentBits)
+const client = new Discord.Client({
+  intents: [ 
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.Guilds,
+           ],
+  partials: [Partials.Channel],
+});
+module.exports = client;
+
+client.commands = new Collection();
+
+['Events', 'Commands'].forEach(handler => {
+  require(`./Handlers/${handler}`)(client);
+})
+
+
+client.login(token);
