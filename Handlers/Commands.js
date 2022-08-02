670e6365445cf4df278441ce3dd49ffbc4dc5f2c
7exe <elliot.singh@outlook.com>
2022-08-02 18:52:25 +0200

    Better handler

--- Handlers/Commands.js
+++ Handlers/Commands.js
@@ -4,9 +4,15 @@ module.exports = (client, Discord) => {
     const command_folder = readdirSync('./Commands');
     for (const folder of command_folder) {
         const command_files = readdirSync(`./Commands/${folder}`).filter(files => files.endsWith('.js'));
+        const commands_array = [];
         for (const file of command_files) {
             const command = require(`../Commands/${folder}/${file}`);
             client.commands.set(command.name, command);
+            commands_array.push(command);
+
+            client.on('ready', () => {
+                client.guilds.cache.get("882378589364944976").commands.set(commands_array);
+            })
         }
     }
 }
