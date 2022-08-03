1ac8bda7dd7ae1c1fd75bb8c187b59e12024a1d4
7exe <elliot.singh@outlook.com>
2022-08-03 17:55:16 +0200

    Added database

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const Discord = require('discord.js');
-const { GatewayIntentBits, Partials, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection, PermissionsBitField} = require('discord.js');
+const Discord = require("discord.js");
+const SQLite = require("better-sqlite3");
+const {
+  GatewayIntentBits,
+  Partials,
+  Routes,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Collection,
+  PermissionsBitField,
+} = require("discord.js");
 
 const { REST } = require("@discordjs/rest");
-const token = process.env['TOKEN'];
-
+const token = process.env["TOKEN"];
 
 //console.log(GatewayIntentBits)
 const client = new Discord.Client({
-  intents: [ 
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent,
-            GatewayIntentBits.DirectMessages,
-            GatewayIntentBits.Guilds,
-           ],
+  intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+  ],
   partials: [Partials.Channel],
 });
 module.exports = client;
@@ -20,9 +29,14 @@ module.exports = client;
 client.commands = new Collection();
 client.buttons = new Collection();
 
-['Events', 'Commands', 'Buttons'].forEach(handler => {
-  require(`./Handlers/${handler}`)(client);
-})
+// Setup database
+const db = new SQLite("./Altix.db");
+//db.prepare('CREATE TABLE IF NOT EXISTS debuglogs (id INTEGER PRIMARY KEY, logs TEXT)').run()
 
+client.database = db;
+
+["Events", "Commands", "Buttons"].forEach((handler) => {
+  require(`./Handlers/${handler}`)(client);
+});
 
 client.login(token);
