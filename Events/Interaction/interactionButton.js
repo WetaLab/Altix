d2b2b55c67c498b0ac14c2cb6587408e651184e8
7exe <elliot.singh@outlook.com>
2022-08-03 23:47:58 +0200

    clean up

--- Events/Interaction/interactionButton.js
+++ Events/Interaction/interactionButton.js
@@ -1,33 +1,39 @@
-const { ButtonInteraction } = require('discord.js');
+const { ButtonInteraction } = require("discord.js");
 
 module.exports = {
   name: "interactionCreate",
   /**
-  * @param {Client} client
-  * @param {ButtonInteraction} interaction
-  **/
+   * @param {Client} client
+   * @param {ButtonInteraction} interaction
+   **/
 
   async execute(interaction, client) {
     if (!interaction.isButton()) return;
 
     const Button = client.buttons.get(interaction.customId.split("-")[0]);
 
-    if (Button.permission && !interaction.member.permissions.has(Button.permission)) {
+    if (
+      Button.permission &&
+      !interaction.member.permissions.has(Button.permission)
+    ) {
       const Error = new EmbedBuilder()
-        .setColor(0xFFFFFF)
+        .setColor(0xffffff)
         .setTitle("Whoa there cowboy!")
-        .setDescription(`You don't have permission to do that!`)
-      return interaction.reply({ embeds: [Error], ephemeral: true })
+        .setDescription(`You don't have permission to do that!`);
+      return interaction.reply({ embeds: [Error], ephemeral: true });
     }
 
-    if (Button.ownerOnly && interaction.member.id !== interaction.guild.ownerId) {
+    if (
+      Button.ownerOnly &&
+      interaction.member.id !== interaction.guild.ownerId
+    ) {
       const Error = new EmbedBuilder()
-        .setColor(0xFFFFFF)
+        .setColor(0xffffff)
         .setTitle("Hm, something isn't quite right!")
-        .setDescription(`You're not the owner of that action!`)
-      return interaction.reply({ embeds: [Error], ephemeral: true })
+        .setDescription(`You're not the owner of that action!`);
+      return interaction.reply({ embeds: [Error], ephemeral: true });
     }
 
     Button.execute(interaction, client);
-  }
-}
+  },
+};
--- Events/Interaction/interactionCreate.js
+++ Events/Interaction/interactionCreate.js
@@ -1,47 +1,52 @@
-const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 module.exports = {
   name: "interactionCreate",
   /**
-  * @param {Client} client
-  * @param {CommandInteraction} interaction
-  **/
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   **/
 
   async execute(interaction, client) {
     if (interaction.isChatInputCommand()) {
-
       const command = client.commands.get(interaction.commandName);
 
-
-
       if (command) {
-
         if (command.permission) {
           const member = interaction.member;
-          if (!member.permissions || !member.permissions.has(command.permission)) {
+          if (
+            !member.permissions ||
+            !member.permissions.has(command.permission)
+          ) {
             const Error = new EmbedBuilder()
-              .setColor(0xFFFFFF)
+              .setColor(0xffffff)
               .setTitle("Whoa there cowboy!")
-              .setDescription(`You do not have permission to run this command!`)
-            return interaction.reply({ embeds: [Error], ephemeral: true })
+              .setDescription(
+                `You do not have permission to run this command!`
+              );
+            return interaction.reply({ embeds: [Error], ephemeral: true });
           }
         }
 
         if (!command) {
-          await interaction.deferReply({ ephemeral: true }).catch(() => { });
+          await interaction.deferReply({ ephemeral: true }).catch(() => {});
         } else {
           if (command.ephemeral) {
-            await interaction.deferReply({ ephemeral: true }).catch(() => { });
+            await interaction.deferReply({ ephemeral: true }).catch(() => {});
           } else {
-            await interaction.deferReply({ ephemeral: false }).catch(() => { });
+            await interaction.deferReply({ ephemeral: false }).catch(() => {});
           }
         }
 
-
-        if (!command) return interaction.followUp({ content: "This command no longer exists.", ephemeral: true }) && client.commands.delete(interaction.commandName);
-
+        if (!command)
+          return (
+            interaction.followUp({
+              content: "This command no longer exists.",
+              ephemeral: true,
+            }) && client.commands.delete(interaction.commandName)
+          );
       }
 
       command.execute(client, interaction);
     }
-  }
-}
+  },
+};
