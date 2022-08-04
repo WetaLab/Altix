744eb9fc16cc857a7f0850546db6a749aade8a2a
7exe <elliot.singh@outlook.com>
2022-08-04 19:24:38 +0200

    Clean up some handler files

--- Handlers/Buttons.js
+++ Handlers/Buttons.js
@@ -1,12 +1,14 @@
-const { readdirSync } = require('fs');
+const { readdirSync } = require("fs");
 
 module.exports = (client, Discord) => {
-  const button_folder = readdirSync('./Buttons');
+  const button_folder = readdirSync("./Buttons");
   for (const folder of button_folder) {
-    const button_files = readdirSync(`./Buttons/${folder}`).filter(files => files.endsWith('.js'));
+    const button_files = readdirSync(`./Buttons/${folder}`).filter((files) =>
+      files.endsWith(".js")
+    );
     for (const file of button_files) {
       const button = require(`../Buttons/${folder}/${file}`);
       client.buttons.set(button.id, button);
     }
   }
-}
+};
--- Handlers/Events.js
+++ Handlers/Events.js
@@ -1,16 +1,22 @@
-const { readdirSync } = require('fs');
+const { readdirSync } = require("fs");
 
 module.exports = (client, Discord) => {
-    const event_folders = readdirSync('./Events');
-    for (const folder of event_folders) {
-        const event_files = readdirSync(`./Events/${folder}`).filter(files => files.endsWith('.js'));
-        for (const file of event_files) {
-            const event = require(`../Events/${folder}/${file}`);
-            if (event.once) {
-                client.once(event.name, (...args) => event.execute(...args, client, Discord));
-            } else {
-                client.on(event.name, (...args) => event.execute(...args, client, Discord));
-            }
-        }
+  const event_folders = readdirSync("./Events");
+  for (const folder of event_folders) {
+    const event_files = readdirSync(`./Events/${folder}`).filter((files) =>
+      files.endsWith(".js")
+    );
+    for (const file of event_files) {
+      const event = require(`../Events/${folder}/${file}`);
+      if (event.once) {
+        client.once(event.name, (...args) =>
+          event.execute(...args, client, Discord)
+        );
+      } else {
+        client.on(event.name, (...args) =>
+          event.execute(...args, client, Discord)
+        );
+      }
     }
-}
+  }
+};
