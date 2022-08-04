ed4b6b77a35ca2e1ea9ed2718f8d2b16e31a917e
7exe <elliot.singh@outlook.com>
2022-08-04 03:06:43 +0200

    Basis for verification handling

--- Buttons/Verify/verify.js
+++ Buttons/Verify/verify.js
@@ -7,6 +7,8 @@ setting commands aswell.
 
 // This is called upon by the "Verify" button
 
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
 module.exports = {
   id: "verifybutton",
   async execute(interaction, client) {
@@ -14,7 +16,7 @@ module.exports = {
       content: "This is the point of which this bot would start your verification process",
       ephemeral: true
     })*/
-    let fetched_role = client.database
+    let server_information = client.database
       .prepare(
         `
 SELECT 
@@ -25,11 +27,63 @@ WHERE
   guildid = ?
     `
       )
-      .get(interaction.guild.id).role;
+      .get(interaction.guild.id);
 
-    interaction.reply({
-      content: fetched_role,
-      ephemeral: true,
-    });
+    // Prepare verification process
+    if (
+      !interaction.member.roles.cache.some(
+        (r) => r.name === server_information.role
+      )
+    ) {
+      // Check permission first
+      if (
+        !interaction.guild.members.me.permissions.has(
+          PermissionsBitField.Flags.Administrator
+        )
+      ) {
+        const Error = new EmbedBuilder()
+          .setColor(0xffffff)
+          .setTitle("Something ain't right here!")
+          .setDescription(
+            `I don't seem to have the proper access to verify you`
+          );
+        return interaction.reply({ embeds: [Error], ephemeral: true });
+      }
+
+      // Check if the thread verification system is needed
+      if (
+        server_information.channel !== -1 &&
+        server_information.questions !== ""
+      ) {
+        // Handle threads
+      } else {
+        // Thread question system has not been setup
+        let role = interaction.guild.roles.cache.find(
+          (r) => r.name === server_information.role
+        );
+        if (!role || role !== undefined || role !== null) {
+          await interaction.member.roles.add(role).then(() => {
+            const Success = new EmbedBuilder()
+              .setColor(0xffffff)
+              .setDescription(`Verification was successful!`);
+            return interaction.reply({ embeds: [Success], ephemeral: true });
+          });
+        } else {
+          const Error = new EmbedBuilder()
+            .setColor(0xffffff)
+            .setTitle("Whoops!")
+            .setDescription(
+              `Uh oh! Seems like the verified role is missing\nYou might want to tell your local admins about this!`
+            );
+          return interaction.reply({ embeds: [Error], ephemeral: true });
+        }
+      }
+    } else {
+      const Error = new EmbedBuilder()
+        .setColor(0xffffff)
+        .setTitle("Something ain't right here!")
+        .setDescription(`You've already been verified!`);
+      return interaction.reply({ embeds: [Error], ephemeral: true });
+    }
   },
 };
